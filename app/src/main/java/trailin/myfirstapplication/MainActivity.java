3b30f62d359d46e12f2c427931027f78046bfd15
LBT76240 <trailin_88_@hotmail.fr>
2017-02-23 15:35:01 +0100

    Video player functional ????

--- app/src/main/java/trailin/myfirstapplication/MainActivity.java
+++ app/src/main/java/trailin/myfirstapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package trailin.myfirstapplication;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,24 +18,31 @@ import android.widget.VideoView;
 import android.net.Uri;
 
 
+import java.util.Map;
+
 import static android.widget.Toast.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener,SurfaceHolder.Callback {
 
     ActionThread actionThread= null;
     MainThread mainThread = MainThread.getInstance();
+    private static String RTSP_URL = "rtp://10.0.1.7:554/play1.sdp";
+    private static MediaPlayer mediaPlayer = new MediaPlayer();
+    private static SurfaceHolder surfaceHolder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.videolayout);
+
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setFixedSize(320, 240);
 
 
-        actionThread = mainThread.getActionThread();
 
-        if(actionThread == null) {
-            onDisconnected();
-        } else {
-            onConnected();
-        }
+
+        //onStart();
 
 
 
@@ -146,6 +157,41 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        actionThread = mainThread.getActionThread();
+
+        if(actionThread == null) {
+            onDisconnected();
+        } else {
+            onConnected();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mainThread.getActionThread()!=null) {
+
+            actionThread.setBite(false);
+            actionThread.disconnect();
+            actionThread = null;
+            mainThread.setActionThread(null);
+
+        }
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        onStart();
+    }
+
+    */
 
     protected void onConnected() {
 
@@ -281,6 +327,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     protected void onDisconnected() {
 
         setContentView(R.layout.connect);
@@ -341,6 +388,41 @@ public class MainActivity extends AppCompatActivity {
         makeText(MainActivity.this, "Mauvaise valeur de : " + string, LENGTH_SHORT).show();
     }
 
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setDisplay(surfaceHolder);
+
+        Context context = getApplicationContext();
+
+        Uri source = Uri.parse(RTSP_URL);
+
+        try {
+            // Specify the IP camera's URL and auth headers.
+            mediaPlayer.setDataSource(context, source);
+
+            // Begin the process of setting up a video stream.
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.prepareAsync();
+        }
+        catch (Exception e) {}
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mediaPlayer.release();
+    }
 
 
 }
